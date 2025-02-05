cc5b9f4d5eca01ecbca8e8a121744a8067674436
what-it-izz <main4izzy@gmail.com>
2025-02-05 15:46:27 -0800

    Move star script to a different file

--- js/index.js
+++ js/index.js
@@ -90,40 +90,9 @@ const initFirstAnimation = () => {
   );
 };
 
-// Function to generate random positions and animations for stars
-function createStar() {
-  const star = document.createElement('div');
-  star.classList.add('star');
-
-  // Random position
-  const x = Math.random() * window.innerWidth;
-  const y = Math.random() * window.innerHeight;
-
-  // Random animation duration
-  const duration = Math.random() * 2 + 1;
-
-  star.style.left = `${x}px`;
-  star.style.top = `${y}px`;
-  star.style.animationDuration = `${duration}s`;
-
-  document.body.appendChild(star);
-};
-
 // Preload images and initialize animations
 preloadImages(".card__img").then(() => {
   document.body.classList.remove("loading");
-
-  // Initialize stars
-  const numStars = 200;
-  for (let i = 0; i < numStars; i++) {
-    createStar();
-  }
-  window.addEventListener('resize', () => {
-    document.querySelectorAll('.star').forEach(star => star.remove());
-    for (let i = 0; i < numStars; i++) {
-      createStar();
-    }
-  });
 
   // Run the intro animation
   initFirstAnimation();
