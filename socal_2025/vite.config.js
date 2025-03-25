78798d5dac8801f3e62f81dd7cbef495d042a7fd
what-it-izz <main4izzy@gmail.com>
2025-03-25 10:39:45 -0700

    fix

--- socal_2025/vite.config.js
+++ socal_2025/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MySite/socal_2025',
+  base: '/MySite/socal_2025/',
 })
